ba0e89641d430fbb8637db3594f0cd63a3371b33
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-04-20 22:58:54 +0800

    Add forum_thread and forum_posts feature

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,6 +1,11 @@
 class ForumThread < ApplicationRecord
   belongs_to :user
   has_many :forum_posts
+
+  accepts_nested_attributes_for :forum_posts
+
+  validates :subject, presence: true
+  validates_associated :forum_posts
 end
 
 # == Schema Information
