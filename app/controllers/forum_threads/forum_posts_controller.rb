5a373d9244d6c5af40203f37e38cf6d7d5bd8ee1
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-04-21 11:31:29 +0800

    Add redirect_to anchor post after create a post

--- app/controllers/forum_threads/forum_posts_controller.rb
+++ app/controllers/forum_threads/forum_posts_controller.rb
@@ -7,7 +7,7 @@ class ForumThreads::ForumPostsController < ForumThreadsController
     @forum_post.user = current_user
 
     if @forum_post.save
-      redirect_to @forum_thread, notice: "Successfully posted!"
+      redirect_to forum_thread_path(@forum_thread, anchor: "forum_post_#{@forum_post.id}"), notice: "Successfully posted!"
     else
       redirect_to @forum_thread, alert: "Unable to save your post!"
     end
